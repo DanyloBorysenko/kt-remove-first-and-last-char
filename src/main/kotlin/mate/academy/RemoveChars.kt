1361a4920363f481f9d920f835bbe9ef68baf1d9
Danylo Borysenko <borysenkodanyl@gmail.com>
2024-05-13 11:17:31 +0300

    fun removeChars was updated

--- src/main/kotlin/mate/academy/RemoveChars.kt
+++ src/main/kotlin/mate/academy/RemoveChars.kt
@@ -1,6 +1,7 @@
 package mate.academy
 
+const val REMOVED_CHARS_COUNT = 1
+
 fun removeChars(str: String): String {
-    // Implement this function
-    return ""
+    return str.drop(REMOVED_CHARS_COUNT).dropLast(REMOVED_CHARS_COUNT)
 }
